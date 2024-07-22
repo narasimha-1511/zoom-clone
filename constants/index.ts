ed26ed8f328c5bdf452557d62d7115f5a4b50209
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-22 17:17:32 +0530

    creating frontend

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const SideBarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imageUrl: "/icons/Home.svg",
+  },
+  {
+    label: "Upcomming",
+    route: "/upcoming",
+    imageUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imageUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imageUrl: "/icons/recording.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imageUrl: "/icons/personal.svg",
+  },
+];
