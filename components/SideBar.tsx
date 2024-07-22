81f9231f0219d2c3c2bc1474ec852b826744109a
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-22 18:15:44 +0530

    creating a responsive nav and sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -12,9 +12,7 @@ const SideBar = () => {
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {SideBarLinks.map((link) => {
-          const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
-
+          const isActive = pathname === link.route;
           return (
             <Link
               href={link.route}
